ecb554a082c29086ce948b81336b6584b80a7678
antuelo <antuel@outlook.com>
2026-03-28 12:20:39 +0100

    envio lo que hice hata ahora

---
+++ CPP01/ex00/Zombie.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: antuel <antuel@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/28 11:53:32 by antuel            #+#    #+#             */
+/*   Updated: 2026/03/28 12:19:37 by antuel           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <iostream>
+#include <string>
+
+class Zombie
+{
+private:
+	std::string _name;
+public:
+	Zombie(std::string name);
+	void announce();
+};
+
+#endif //ZOMBIE_HPP
