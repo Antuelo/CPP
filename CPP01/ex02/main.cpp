718144da5322aa66a434302fa73cc5fda51f0968
antuelo <antuel@outlook.com>
2026-04-14 17:18:26 +0200

    modifique el nombre del zombie

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -6,27 +6,16 @@
 /*   By: antuel <antuel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/28 11:53:23 by antuel            #+#    #+#             */
-/*   Updated: 2026/04/14 17:16:23 by antuel           ###   ########.fr       */
+/*   Updated: 2026/04/12 18:43:45 by antuel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Zombie.hpp"
-
-Zombie* zombieHorde(int N, std::string name);
+#include <iostream>
+#include <string>
 
 int main(void)
 {
-    int N = 12;
-	int i;
-
-	std::cout << "the horde of " << N << " attack! run away !!!!" << std::endl;
-	Zombie *horde = zombieHorde(N, "Michale Jackson");
-
-	for (i = 0; i < N; i++)
-	{
-		horde[i].announce();
-	}
-	delete[] horde;
-	std::cout << "\nthe zombies are dead ... again\n" << std::endl;
-	return (0);
+    std::string text = "HI THIS IS BRAIN";
+	std::string stringPTR = &text;
+	std::string stringREF& = text;
 }
