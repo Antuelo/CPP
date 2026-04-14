65ad9193ed53ae058a2a128b2fd99c9e551218b6
antuelo <antuel@outlook.com>
2026-04-14 17:42:08 +0200

    termine el ex02

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: antuel <antuel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/28 11:53:23 by antuel            #+#    #+#             */
-/*   Updated: 2026/04/12 18:43:45 by antuel           ###   ########.fr       */
+/*   Updated: 2026/04/14 17:40:00 by antuel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,18 @@
 int main(void)
 {
     std::string text = "HI THIS IS BRAIN";
-	std::string stringPTR = &text;
-	std::string stringREF& = text;
+	std::string* stringPTR = &text;
+	std::string& stringREF = text;
+
+	std::cout << "\nAddresses are: " << std::endl;
+	std::cout << &text << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
+
+	std::cout << "\n\nValues are: " << std::endl;
+	std::cout << text << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
+
+	return 0;
 }
