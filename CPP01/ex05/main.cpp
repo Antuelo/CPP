b55efa6e6aee3bd5b569f18b272ec0c50411e9f4
antuelo <antuel@outlook.com>
2026-04-22 18:44:33 +0200

    terminando el ex05 CPP01

--- CPP01/ex05/main.cpp
+++ CPP01/ex05/main.cpp
@@ -6,7 +6,31 @@
 /*   By: antuel <antuel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/21 09:06:39 by antuel            #+#    #+#             */
-/*   Updated: 2026/04/21 09:06:40 by antuel           ###   ########.fr       */
+/*   Updated: 2026/04/22 18:43:21 by antuel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Harl.hpp"
+
+int main()
+{
+	Harl harl;
+	
+	std::cout << "Harl say: \n" << std::endl;
+
+	harl.complain("INFO");
+	std::cout << std::endl;
+
+	harl.complain("WARNING");
+	std::cout << std::endl;
+
+	harl.complain("DEBUG");
+	std::cout << std::endl;
+	
+	harl.complain("ERROR");
+	std::cout << std::endl;
+
+	harl.complain("other thing");
+	
+	return(0);
+}
