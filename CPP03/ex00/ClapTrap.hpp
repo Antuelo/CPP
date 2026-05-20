858cf7e39c1d110ffa148e6c099633a2ec5c5e2a
Antuelo <antuel@outloook.com>
2026-05-20 11:11:46 +0200

    maj

--- CPP03/ex00/ClapTrap.hpp
+++ CPP03/ex00/ClapTrap.hpp
@@ -11,8 +11,8 @@ class ClapTrap
 		int 			_hit_points;
 		int				_attack_damage;
 		int 			_energy_points;
-		
-	
+
+
 	public:
 		//const
 		ClapTrap();
@@ -23,11 +23,11 @@ class ClapTrap
 
 		//oper
 		ClapTrap& operator=(const ClapTrap &other);
-	
+
 		//method
 	void 	attack(const std::string &target);
-	void 	takeDamage(int amount);
-	void 	beRepaired(int amount);
+	void 	takeDamage(unsigned int amount);
+	void 	beRepaired(unsigned int amount);
 
 	int		get_hitpoints()const;
 	int		get_energypoints()const;
--- CPP03/ex00/main.cpp
+++ CPP03/ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: antuel <antuel@student.42.fr>              +#+  +:+       +#+        */
+/*   By: anoviedo <antuel@outlook.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/14 16:34:38 by antuel            #+#    #+#             */
-/*   Updated: 2026/05/14 19:38:55 by antuel           ###   ########.fr       */
+/*   Updated: 2026/05/20 11:11:15 by anoviedo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,27 +16,27 @@ int main(void)
 {
     ClapTrap player1("Michel");
     ClapTrap player2("Jackson");
-    
+
     player1.attack("ennemi");
-    
-    player2.takeDamage(-3);
-    
+
+    player2.takeDamage(3);
+
     player2.beRepaired(5);
-    
+
     for (int i = 0; i < 11; i++)
 	{
         player1.attack("sac d'entraînement");
 		if (player1.get_energypoints() == 0)
 			break;
     }
-    
+
     player1.attack("ennemi");
-    
+
     player2.takeDamage(100);
     player2.beRepaired(10);
-    
+
     player2.attack("ennemi");
-    
+
     return (0);
 }
 
@@ -45,16 +45,16 @@ int main(void)
 {
 	ClapTrap player1("Michael Jackson");
 	ClapTrap player2("ta soeur");
-	
+
 	player1.attack("singing");
 	player2.takeDamage(5);
-	
+
 	player2.attack("dancing");
 	player1.takeDamage(1);
 
 	player1.attack("dancing and singing");
 	player2.takeDamage(15351);
-	
+
 	player1.beRepaired(55);
 
 	return (0);
