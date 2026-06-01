f570d164cfb4ce940f884347952a55fb6978e254
antuelo <antuel@outlook.com>
2026-06-01 22:40:56 +0200

    maj

--- CPP04/ex01/main.cpp
+++ CPP04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: antuel <antuel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/22 15:24:45 by antuel            #+#    #+#             */
-/*   Updated: 2026/06/01 22:36:37 by antuel           ###   ########.fr       */
+/*   Updated: 2026/06/01 22:39:38 by antuel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,11 @@ int main()
 {
 	const	Animal* j = new Dog();
 	const 	Animal* i = new Cat();
-
+	
+	std::cout << std::endl;
+	std::cout << std::endl;
+	std::cout << std::endl;
+	
 	delete j;//should not create a leak
 	delete i;
 	
@@ -29,6 +33,9 @@ int main()
 		else
 			animals[i] = new Cat();
 	}
+	std::cout << std::endl;
+	std::cout << std::endl;
+	std::cout << std::endl;
 	for (int i = 0; i < 10; i++)
 		delete animals[i];
 	
