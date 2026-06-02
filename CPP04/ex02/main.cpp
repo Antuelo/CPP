2598ea3a267163f2c4cc04a2ac36eb85207b5cb3
antuelo <antuel@outlook.com>
2026-06-02 23:46:43 +0200

    ya veré mañana

--- CPP04/ex02/main.cpp
+++ CPP04/ex02/main.cpp
@@ -6,51 +6,101 @@
 /*   By: antuel <antuel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/22 15:24:45 by antuel            #+#    #+#             */
-/*   Updated: 2026/06/02 15:04:06 by antuel           ###   ########.fr       */
+/*   Updated: 2026/06/02 23:36:49 by antuel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "animal.hpp"
 #include "WrongAnimal.hpp"
 
+
+//EXEMPLE DE PAS POUVOIR FAIRE
+/*
 int main()
 {
-	const	Animal* j = new Dog();
-	const 	Animal* i = new Cat();
-	
-	std::cout << std::endl;
-	std::cout << std::endl;
-	std::cout << std::endl;
-	
-	delete j;//should not create a leak
-	delete i;
-	
-	Animal *animals[10];
-	for(int i = 0; i < 10; i++)
 	{
-		if (i < 5)
-			animals[i] = new Dog();
-		else
-			animals[i] = new Cat();
+		//defaul
+		const Animal *bestiole = new Animal();
+		//copy
+		const Animal *autrebestiole = new Animal(*bestiole);
+		//inherits (with parameter made in the children class)
+		const Animal *gato = new Cat();
+		const Animal *perro = new Dog();
+
+		std::cout << "el gato es: a " << gato->getType() << std::endl;
+		std::cout << "el perro es: a " << perro->getType() << std::endl;
+		//methods
+		std::cout << "the cat say: ";
+		gato->makeSound();
+		std::cout << "The dog say: ";
+		perro->makeSound();
+
+		std::cout << "An animal makes a strange sound: ";
+		bestiole->makeSound();
+
+		std::cout << "An other strange animal copy the strange animal: ";
+		autrebestiole->makeSound();
+		
+		delete bestiole;
+		delete autrebestiole;
+		delete gato;
+		delete perro;
 	}
-	std::cout << std::endl;
-	std::cout << std::endl;
-	std::cout << std::endl;
-	for (int i = 0; i < 10; i++)
-		delete animals[i];
-	
-	std::cout << std::endl;
-	std::cout << "copy test time-----------------------\n" << std::endl;
-	
-	Dog *Perro = new Dog();
-	Dog *Pitbul = new Dog(*Perro);
-	
-	std::cout << std::endl;
-	std::cout << "DESTRUCTION .............>" << std::endl;
-	std::cout << std::endl;
-	
-	delete Perro;
-	delete Pitbul;
-	
-	return 0;
+	//wrong animals examples
+	{
+		std::cout << "\n-------------- Wrong animal time ----------------------\n" << std::endl;
+		const WrongAnimal 	*strange 				= new WrongAnimal();
+		const WrongAnimal	*this_is_a_wrongCat 	= new WrongCat();
+		const WrongAnimal 	*isnotadog				= new WrongDog();
+
+		std::cout << "\nA strange cat say: ";
+		this_is_a_wrongCat->makeSound();
+
+		std::cout << "\nThis is'n a Dog! listen the sound: ";
+		isnotadog->makeSound();		
+		
+		std::cout << "\nSo, finaly... a wrong-animal say: ";
+		strange->makeSound();
+
+		std::cout << std::endl;
+		
+		delete strange;
+		delete this_is_a_wrongCat;
+		delete isnotadog;
+	}
+	return (0);
+}*/
+
+
+int main()
+{
+
+		//defaul
+		//const Animal *bestiole = new Animal();
+		//copy
+		//const Animal *autrebestiole = new Animal(*bestiole);
+		//inherits (with parameter made in the children class)
+		const Animal *gato = new Cat();
+		const Animal *perro = new Dog();
+
+		std::cout << "el gato es: a " << gato->getType() << std::endl;
+		std::cout << "el perro es: a " << perro->getType() << std::endl;
+		//methods
+		std::cout << "the cat say: ";
+		gato->makeSound();
+		std::cout << "The dog say: ";
+		perro->makeSound();
+
+		std::cout << "An animal makes a strange sound: ";
+//		bestiole->makeSound();
+
+		std::cout << "An other strange animal copy the strange animal: ";
+//		autrebestiole->makeSound();
+		
+//		delete bestiole;
+//		delete autrebestiole;
+		delete gato;
+		delete perro;
+		
+	return (0);
 }
