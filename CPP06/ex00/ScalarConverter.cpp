4aac8782872f70f38bac632f45e4d58e59f7cb8d
antuelo <antuel@outlook.com>
2026-07-21 14:57:14 +0200

    maj de cpp06

--- CPP06/ex00/ScalarConverter.cpp
+++ CPP06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: antuel <antuel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/19 18:24:12 by antuel            #+#    #+#             */
-/*   Updated: 2026/07/19 18:28:27 by antuel           ###   ########.fr       */
+/*   Updated: 2026/07/21 14:35:29 by antuel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,3 +23,21 @@ ScalarConverter::ScalarConverter(const ScalarConverter &copy)
 
 ScalarConverter::~ScalarConverter()
 {}
+
+void ScalarConverter::convert(std::string param)
+{
+	int length = 0;
+
+	length = param.length();
+	
+	if (param == "")
+		return ;
+	else if (param.length() == 3 && param[0] == '\'' && param[2] == '\'')
+		return ;//es char
+	else if (param[length] == 'f' && param.find('.') != std::string::npos && param[0] != '.')
+		return ;//es un float
+	else if (param[length] == 'f')
+		return ;//es double
+	else
+		return ;//es int
+}
--- CPP06/ex00/ScalarConverter.hpp
+++ CPP06/ex00/ScalarConverter.hpp
@@ -19,3 +19,22 @@ class ScalarConverter
 };
 
 #endif //SCALARCONVERTER_HPP
+
+
+/*
+	el principio es que no tiene que ser instanciable, es decir, no necesita un objeto, se le pasa el dato directamente
+
+ejemplo: 
+
+❌ Si fuera instanciable (como está ahora):
+
+ScalarConverter obj;               // ← se puede crear un objeto
+obj.convert("42");                 // ← se puede llamar al método
+
+✅ Si NO es instanciable (como debe ser):
+cpp
+
+ScalarConverter::convert("42");    // ← se llama directamente sin objeto
+
+Ventaja: Más limpio, más eficiente, evita confusiones.
+*/
